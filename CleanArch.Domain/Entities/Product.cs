f3bf8615b7c80c7652d0985b1affbaa57402beb7
Willian Yuiti Watanabe <willian.wtnb94@gmail.com>
2023-12-26 21:40:52 -0300

    Initial commit

---
+++ CleanArch.Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+
+    }
+}
+}
