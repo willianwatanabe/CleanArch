85e030ebffd853287414c4460ee034901d2b5732
Willian Yuiti Watanabe <willian.wtnb94@gmail.com>
2023-12-31 13:08:28 -0300

    Implementando o AutoMapper e realizando mapeamento das entidades para DTOs

---
+++ CleanArch.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,44 @@
+﻿using CleanArch.Domain.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanArch.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "The Description is required")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "The Price is required")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Price")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "The Price is required")]
+        [Range(1, 9999)]
+        [DisplayName("Stock")]
+        public int Stock { get; set; }
+
+        [DisplayName("Image")]
+        [MaxLength(250)]
+        public string Image { get; set; } = string.Empty;
+
+        public Category? Category { get; set; }
+
+        [DisplayName("Categories")]
+        public int CategoryId { get; set; }
+
+    }
+}
---
+++ CleanArch.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CleanArch.Application.DTOs;
+using CleanArch.Domain.Entities;
+
+namespace CleanArch.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
