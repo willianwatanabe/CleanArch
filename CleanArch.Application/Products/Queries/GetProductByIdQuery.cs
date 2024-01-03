b19747e7919c5e6752f2c2c3bdda19121f3368fc
Willian Yuiti Watanabe <willian.wtnb94@gmail.com>
2024-01-02 21:33:56 -0300

    Introduzindo CQRS
    Adicionando Commands e Queries

---
+++ CleanArch.Application/Products/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using CleanArch.Application.Products.Commands;
+using CleanArch.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Products.Queries
+{
+    public class GetProductByIdQuery : IRequest<Product>
+    {
+        public int Id { get; set; }
+        public GetProductByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
