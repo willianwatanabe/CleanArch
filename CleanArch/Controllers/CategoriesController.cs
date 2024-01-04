69a076f634717a5e4df57e766691cb04d8db33f0
Willian Yuiti Watanabe <willian.wtnb94@gmail.com>
2024-01-04 20:26:29 -0300

    Metodo editar categoria

--- CleanArch/Controllers/CategoriesController.cs
+++ CleanArch/Controllers/CategoriesController.cs
@@ -29,9 +29,35 @@ namespace CleanArch.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CategoryDTO category)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 await _categoryServices.AddAsync(category);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id is null)
+                return NotFound();
+
+            var category = await _categoryServices.GetByIdAsync(id.Value);
+
+            if (category is null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CategoryDTO category)
+        {
+            if (ModelState.IsValid)
+            {
+                await _categoryServices.UpdateAsync(category);
                 return RedirectToAction(nameof(Index));
             }
 
