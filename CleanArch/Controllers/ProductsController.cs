ceed54e39d4916bf082daa2947ef7dd43ce45b89
Willian Yuiti Watanabe <willian.wtnb94@gmail.com>
2024-01-06 16:50:18 -0300

    Metodo criar produto

--- CleanArch/Controllers/ProductsController.cs
+++ CleanArch/Controllers/ProductsController.cs
@@ -1,15 +1,20 @@
-﻿using CleanArch.Application.Interfaces;
+﻿using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArch.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductServices _productServices;
+        private readonly ICategoryServices _categoryServices;
 
-        public ProductsController(IProductServices productServices)
+        public ProductsController(IProductServices productServices, ICategoryServices categoryServices)
         {
             _productServices = productServices;
+            _categoryServices = categoryServices;
         }
 
         [HttpGet]
@@ -18,5 +23,26 @@ namespace CleanArch.Controllers
             var products = await _productServices.GetProductsAsync();
             return View(products);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId = new SelectList(await _categoryServices.GetCategoriesAsync(), "ID", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productServices.AddAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(product);
+        }
+
     }
 }
